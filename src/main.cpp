5b528537f338b1ccb3c8ecc94ab73a272c4af27a
iMac MrHogun <s.j.000abk@gmail.com>
2025-08-17 23:30:57 +0300

    feat(main): call fetchWeather in setup to retrieve weather data

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
 
 #include "sensors/bme680_sensor.h"
+#include "weather/api/weather_api.h"
 #include "wifi/wifi_manager.h"
 
 void setup() {
   Serial.begin(115200);
   connectToWiFi();
+  fetchWeather();
   bme680Init();
 }
 
 void loop() {
   bme680Update();
-
+  Serial.println("---------------------------");
   Serial.print("Температура: ");
   Serial.print(bme680Temperature);
   Serial.println(" °C");
