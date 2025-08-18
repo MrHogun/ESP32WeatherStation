af653bc337c28184b5f3553a5ffbbe9dc31e6105
iMac MrHogun <s.j.000abk@gmail.com>
2025-08-18 14:36:28 +0300

    feat(mqtt): integrate MQTT publishing for BME680 sensor data in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+#include "mqtt/mqtt_manager.h"
 #include "sensors/bme680_sensor.h"
 #include "weather/api/weather_api.h"
 #include "wifi/wifi_manager.h"
@@ -7,16 +8,21 @@
 void setup() {
   Serial.begin(115200);
   connectToWiFi();
-  fetchWeather();
+  // fetchWeather();
   bme680Init();
+  mqttInit();
 }
 
 void loop() {
   bme680Update();
+  publishFloat("home/esp32weather/temperature", bme680Temperature);
+  publishFloat("home/esp32weather/humidity", bme680Humidity);
+  publishFloat("home/esp32weather/pressure", bme680Pressure);
+  publishFloat("home/esp32weather/iaq", bme680Iaq);
+  mqttLoop();
+
+  Serial.println("----------------------------------");
   printBme680Data();
-  Serial.println("----------------------------------");
-  fetchWeather();
-  Serial.println("----------------------------------");
 
   delay(60000);
 }
